d9230615bf0a052d359e02bfd499e24796bc3a10
Monica <Monica@localhost>
2018-04-12 11:48:14 +0300

    Coordinate class for pieces

---
+++ AI_Project2/src/ai_project2/Coordinate.java
@@ -0,0 +1,59 @@
+package ai_project2;
+
+public class Coordinate {
+    private int row;
+    private int col;
+
+    public Coordinate(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.row;
+        hash = 53 * hash + this.col;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordinate other = (Coordinate) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.col != other.col) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+    
+    
+    
+}
